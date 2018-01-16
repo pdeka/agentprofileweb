7e7af564d1895ce4b325862c4b39b0bee5cca051
pdeka <prabindeka@gmail.com>
2018-01-16 21:58:23 +1100

    Map container working at the moment

---
+++ src/MapContainer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { compose, withProps } from "recompose";
+import {withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const latitude = -33.718804;
+const longitude = 150.919947;
+const mapStyles =   [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}];
+const mapOptions = {
+  scrollwheel: false,
+  styles: mapStyles
+};
+
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDSFa3Pp1iG6Sh3YwZvHnOnVSeR5Qn0oWA",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={15}
+    defaultCenter={{ lat: latitude, lng: longitude - 0.020}}
+    defaultOptions={{mapOptions}}
+  >
+    <Marker position={{ lat: latitude, lng: longitude }} />
+  </GoogleMap>
+);
+
+export default class MapContainer extends React.Component {
+
+    state = {
+      doc: null,
+      notFound: false,
+    }
+
+    render() {
+      return <MyMapComponent />;
+    }
+}
