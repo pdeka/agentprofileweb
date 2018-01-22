cb12fc46f24d76701e31344ace4be820a7fc834e
pdeka <prabindeka@gmail.com>
2018-01-22 22:08:38 +1100

    Setting up the routes correctly.

--- src/MainNavigation.js
+++ src/MainNavigation.js
@@ -38,12 +38,12 @@ export default class MainNavigation extends React.Component {
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="/home" class="nav-link">
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a href="#pablo" class="nav-link">
+              <a href="/articlesvideos" class="nav-link">
                 Articles & Videos
               </a>
             </li>
