1a1ce6d0b69d3887480b9ba2090e5f439dd8b49a
pdeka <prabindeka@gmail.com>
2018-01-31 01:01:22 +1100

    Added some content

--- src/About.js
+++ src/About.js
@@ -60,11 +60,10 @@ export default class About extends React.Component {
 
       return <div class="sections-page">
         <MainNavigation thisProp={data} navBarTransparent={true}/>
-        <div class="page-header header-filter header-small" data-parallax="true" style={{backgroundImage: 'url(https://prismic-io.s3.amazonaws.com/rumamundi%2F299afe9b-2b94-4fe8-a7c1-99dbeabee565_ruma-in-group.jpg)'}}>
+        <div class="page-header header-small" data-parallax="true" style={{backgroundImage: 'url(https://prismic-io.s3.amazonaws.com/rumamundi%2F299afe9b-2b94-4fe8-a7c1-99dbeabee565_ruma-in-group.jpg)'}}>
           <div class="container">
             <div class="row">
               <div class="col-md-8 ml-auto mr-auto text-center">
-                <h2 class="title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
               </div>
             </div>
           </div>
@@ -74,73 +73,39 @@ export default class About extends React.Component {
               <div class="features-1 no-padding-bottom">
                   <div class="row">
                       <div class="col-md-8 ml-auto mr-auto">
-                          <h2 class="title">Meet Ruma</h2>
-                          <h5 class="description">
-                            Behind Ruma’s personable manner and respectful approach is a sharp and strategic thinker with exemplary sales skills. With nearly 20 years’ experience in the real estate industry, Ruma has honed many highly effective negotiating strategies that time and again have produced exceptional sale prices for her clients. Displaying absolute integrity and professionalism, Ruma’s dedication to each individual campaign is reflected in the consistency of her results.
+                          <h2 class="title">Serving you with love and care</h2>
+                          <h5 class="description"  style={{'color': 'black'}}>
+                            In the past decade, Ruma Mundi's name has become synonymous with Real Estate in the Hills District.  Behind her personable manner and respectful approach is a sharp and strategic thinker with exemplary sales skills. With nearly 20 years’ experience in the real estate industry, Ruma has honed highly effective negotiating strategies that time and again have produced exceptional sale prices for her clients. Displaying absolute integrity and professionalism, Ruma’s dedication to each individual campaign is reflected in the consistency of her results.
+                          </h5>
+                          <h5 class="description"  style={{'color': 'black'}}>
+                            She is a hard working mom of 2 beautiful daughters, Samara and Safira, which among other things, gives her a perspective on what a growing family needs. Nick, her husband helps her out between his work as an executive at Woolworths, and making time for Samara and Safira. Ruma is keenly aware of the juggles of everyday life in the North West of Sydney, and the pressures families today have to work with to live happy and stay healthy.
+                          </h5>
+                          <h5 class="description"  style={{'color': 'black'}}>
+                            Principal of First National Hills Direct, Ruma is a hard-working, dedicated director who has an unwavering dedication for her Agency and her staff.  With an Master’s degree in business administration and a bachelor’s degree in commerce, she has the education to back her up.  Ruma and her team's success are driven by having a strong market knowledge of the Hills District and having a passion for people coupled with a persona that is caring and approachable. During her dynamic career, Ruma has succeeded in bringing about a positive and refreshing change to customer service in the real estate industry, not just in the North West of Sydney, but in the country as a whole.
                           </h5>
                           <div class="icon">
                             <i class="material-icons">format_quote</i>
                           </div>
                           <blockquote class="blockquote text-center">
-                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                            <p class="mb-0">
+                              I have been selling real estate on Sydney’s Hills District for nearly 20 years. A major portion of the homes that I sell are either past clients, or referrals. My passion and motivation is continually driven forward by the satisfaction I receive when I secure an exceptional outcome for my clients.
+                            </p>
                             <footer class="blockquote-footer">Ruma Mundi</footer>
                           </blockquote>
                       </div>
                   </div>
               </div>
           </div>
-          <div class="team-2 section-dark" id="team-5" >
+          <div class="team-2 section-image" id="team-5" style={{'backgroundImage': 'url(https://prismic-io.s3.amazonaws.com/rumamundi%2F299afe9b-2b94-4fe8-a7c1-99dbeabee565_ruma-in-group.jpg)'}}>
               <div class="container">
                   <div class="row">
                       <div class="col-md-8 ml-auto mr-auto text-center">
                           <h2 class="title">Awards and Recognitions</h2>
-                          <h5 class="description" style={{'marginBottom': 0}}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                          <h5 class="description">
+                            While it is an honour&nbsp;to be recognised both nationally and globally, my true reward comes from the satisfaction that I gain from a good result for my clients.&nbsp;
+                            <br/>
+                          - Ruma
                           </h5>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="team-2" id="team-2">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-md-4">
-                          <div class="card card-profile card-plain">
-                              <div class="card-header card-header-image" style={{'height': '198px'}}>
-                                  <a href="#pablo">
-                                      <img class="img" src="./images/REB-top-50.png" style={{'height': '198px'}} alt="REB top 50 Ruma Mundi North West Sydney"/>
-                                  </a>
-                              </div>
-                              <div class="card-body ">
-                                  <h4 class="card-title">#29 | REB Awards, 2017</h4>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="col-md-4">
-                          <div class="card card-profile card-plain">
-                              <div class="card-header card-header-image" style={{'height': '198px'}}>
-                                  <a href="#pablo">
-                                      <img class="img" src="./images/First-National-Real-Estate-SEQA-441x269.png" style={{'height': '198px'}} alt="First National Real Estate Ruma Mundi Sydney"/>
-                                  </a>
-                              </div>
-                              <div class="card-body ">
-                                  <h4 class="card-title">#2 | Combined Highest Gross Sales</h4>
-                                  <h6 class="card-category text-muted">First National Real Estate NSW Network</h6>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="col-md-4">
-                          <div class="card card-profile card-plain">
-                              <div class="card-header card-header-image" style={{'height': '198px'}}>
-                                  <a href="#pablo">
-                                      <img class="img" src="./images/REBsalesagentoftheyear.jpeg" style={{'height': '198px'}} alt="REB sales agent of the year Ruma Mundi Sydney"/>
-                                  </a>
-                              </div>
-                              <div class="card-body ">
-                                  <h4 class="card-title">Finalist | Sales Agent of the Year - Metropolitan </h4>
-                                  <h6 class="card-category text-muted">REB Awards 2017</h6>
-                              </div>
-                          </div>
                       </div>
                   </div>
                   <div class="row">
@@ -232,9 +197,22 @@ export default class About extends React.Component {
                                   <i class="fa fa-trophy"></i>
                               </div>
                               <div class="description">
-                                  <h4 class="info-title">Bronze Auction Certificate</h4>
+                                  <h4 class="info-title">Grand Centurion</h4>
                                   <p>
-                                    2nd Quarter 2015
+                                    Top 1% Producer Globally, 2015
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                          <div class="info info-horizontal" style={{'padding': 0}}>
+                              <div class="icon icon-default">
+                                  <i class="fa fa-trophy"></i>
+                              </div>
+                              <div class="description">
+                                  <h4 class="info-title">Silver Auction Certificate</h4>
+                                  <p>
+                                    3rd Quarter 2015
                                   </p>
                               </div>
                           </div>
@@ -247,25 +225,55 @@ export default class About extends React.Component {
                               <div class="description">
                                   <h4 class="info-title">Top Principal (GCC) NSW</h4>
                                   <p>
-                                    1st Quarter 2015
-                                  </p>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="col-md-4">
-                          <div class="info info-horizontal" style={{'padding': 0}}>
-                              <div class="icon icon-default">
-                                  <i class="fa fa-trophy"></i>
-                              </div>
-                              <div class="description">
-                                  <h4 class="info-title">Top Residential Principal</h4>
-                                  <p>
-                                    (Units) NSW - 1st Quarter 2015
+                                    2nd Quarter 2015
                                   </p>
                               </div>
                           </div>
                       </div>
                   </div>
+                  <div class="row">
+                      <div class="col-md-4">
+                          <div class="card card-profile card-plain">
+
+                              <div class="card-header card-header-image" style={{'height': '166px'}}>
+                                  <a href="#pablo">
+                                      <img class="img" src="./images/REB-top-50.png" style={{'height': '166px'}} alt="REB top 50 Ruma Mundi North West Sydney"/>
+                                  </a>
+                              </div>
+                              <div class="card-body ">
+                                  <h4 class="card-title">#29 | REB Awards, 2017</h4>
+                              </div>
+
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                          <div class="card card-profile card-plain">
+                              <div class="card-header card-header-image" style={{'height': '166px'}}>
+                                  <a href="#pablo">
+                                      <img class="img" src="./images/First-National-Real-Estate-SEQA-441x269.png" style={{'height': '166px'}} alt="First National Real Estate Ruma Mundi Sydney"/>
+                                  </a>
+                              </div>
+                              <div class="card-body ">
+                                  <h4 class="card-title">#2 | Combined Highest Gross Sales</h4>
+                                  <h6 class="card-category text-muted">First National Real Estate NSW Network</h6>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                          <div class="card card-profile card-plain">
+                              <div class="card-header card-header-image" style={{'height': '166px'}}>
+                                  <a href="#pablo">
+                                      <img class="img" src="./images/REBsalesagentoftheyear.jpeg" style={{'height': '166px'}} alt="REB sales agent of the year Ruma Mundi Sydney"/>
+                                  </a>
+                              </div>
+                              <div class="card-body ">
+                                  <h4 class="card-title">Finalist | Sales Agent of the Year - Metropolitan </h4>
+                                  <h6 class="card-category">REB Awards 2017</h6>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
               </div>
           </div>
           <FooterLinkedToContactUs data={aboutpageResults[0].data.footer_content[0].text}/>
