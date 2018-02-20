482b2a485efe59a5bba2934bfdc56685a00129b3
pdeka <prabindeka@gmail.com>
2018-02-21 01:13:51 +1100

    created a footer.

--- src/partials/PageFooter.js
+++ src/partials/PageFooter.js
@@ -7,11 +7,41 @@ import "../styles/css/PageFooter.css";
 export default class PageFooter extends React.Component {
 
   render() {
-    return <footer className={classNames('footer')}>
-          <div className={classNames('container')}>
-              <nav className={classNames('float-left')}>
-              </nav>
-              <div className={classNames('copyright', 'float-left')}>
+    return <footer className={classNames('footer','footer-white')}>
+          <div class="container">
+              <ul class="social-buttons">
+                  <li>
+                      <a href="#pablo" class="btn btn-just-icon btn-link btn-twitter" target="_blank">
+                          <i class="fa fa-twitter"  style={{'fontSize': '24px'}} ></i>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#pablo" class="btn btn-just-icon btn-link btn-facebook"  target="_blank">
+                          <i class="fa fa-facebook-square"  style={{'fontSize': '24px'}}></i>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#pablo" class="btn btn-just-icon btn-link btn-linkedin"  target="_blank">
+                          <i class="fa fa-linkedin"  style={{'fontSize': '24px'}}></i>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#pablo" class="btn btn-just-icon btn-link btn-instagram"  target="_blank">
+                          <i class="fa fa-instagram"  style={{'fontSize': '24px'}}></i>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#pablo" class="btn btn-just-icon btn-link btn-youtube"  target="_blank">
+                          <i class="fa fa-youtube-play"  style={{'fontSize': '24px'}}></i>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#pablo" class="btn btn-just-icon btn-link btn-youtube"  target="_blank">
+                          <i class="fa icon-ratemyagent"  style={{'fontSize': '18px', 'color': '#37474f'}}></i>
+                      </a>
+                  </li>
+              </ul>
+              <div className={classNames('copyright', 'pull-center')}>
                 &copy; 2018, made with &nbsp;
                 <i className={classNames('material-icons')}>favorite</i>
                 &nbsp; by &nbsp;
