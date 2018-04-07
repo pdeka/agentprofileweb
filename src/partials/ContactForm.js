4e0b5a0e862a926f877b04f778f6fa5cfbe41dec
pdeka <prabindeka@gmail.com>
2018-04-07 16:01:27 +1000

    Here is the contact form

--- src/partials/ContactForm.js
+++ src/partials/ContactForm.js
@@ -10,6 +10,8 @@ export default class ContactForm extends React.Component {
         this.state = {
             send: false,
             email: '',
+            phone: '',
+            purpose: '',
             text: '',
             to: 'prabin@kenekt.com.au',
             subject: 'Mail Form From Website'
@@ -19,6 +21,18 @@ export default class ContactForm extends React.Component {
     updateEmail (evt) {
         this.setState({
             email: evt.target.value
+        });
+    }
+
+    updatePhone (evt) {
+        this.setState({
+            phone: evt.target.value
+        });
+    }
+
+    updatePurpose (evt) {
+        this.setState({
+            purpose: evt.target.value
         });
     }
 
@@ -34,6 +48,15 @@ export default class ContactForm extends React.Component {
           return true;
         } else {
           $('#robotCheckContent').addClass('has-danger')
+          return false;
+        }
+    }
+
+    isPurposeSelected() {
+        if ($('input[name=purpose]:checked').val()) {
+          return true;
+        } else {
+          $('#purposeContent').addClass('has-danger')
           return false;
         }
     }
@@ -58,16 +81,28 @@ export default class ContactForm extends React.Component {
         }
     }
 
+    isPhoneValid() {
+        if (this.state.phone && this.state.phone.match(/\d/g) && this.state.phone.match(/\d/g).length === 10) {
+          return true;
+        } else {
+          $('#phoneContent').addClass('has-danger');
+          return false;
+        }
+    }
+
 
     _handleSubmit(event)  {
         event.preventDefault();
 
-        if (this.isEmailValid() && this.isARobotChecked() && this.isMessageValid()) {
+        if (this.isEmailValid() && this.isPhoneValid()  && this.isMessageValid() && this.isPurposeSelected() && this.isARobotChecked() ) {
             $('#emailId').removeClass('has-danger').addClass('has-success');
             $('#robotCheckContent').removeClass('has-danger').addClass('has-success')
             $('#emailContent').removeClass('has-danger').addClass('has-success');
+            $('#phoneContent').removeClass('has-danger').addClass('has-success');
+            $('#purposeContent').removeClass('has-danger').addClass('has-success');
             $('#success').addClass('form-control-feedback');
-            console.log("sending message!");
+            $('#submitButton').attr('disabled', 'true');
+            console.log("sending message!: " + JSON.stringify(this.state));
             this.sendMail(this.state);
         }
       }
@@ -105,12 +140,49 @@ export default class ContactForm extends React.Component {
                         onFocus={this.inputFocus("#email-label")} />
                       <span className={classNames('material-input')}></span>
                   </div>
+                  <div id="phoneContent" className={classNames('form-group', 'label-floating', 'is-empty', 'bmd-form-group')}>
+                      <label htmlFor="phone" className={classNames('bmd-label-placeholder')} id="phone-label" >Your Phone (10 Digits)</label>
+                      <input id="phone" type="phone" value={this.state.phone} onChange={this.updatePhone.bind(this)} className={classNames('form-control')}
+                        onFocus={this.inputFocus("#phone-label")} />
+                      <span className={classNames('material-input')}></span>
+                  </div>
                   <div id="emailContent" className={classNames('form-group', 'label-floating', 'is-empty', 'bmd-form-group')}>
                       <label htmlFor="exampleMessage1" className={classNames('bmd-label-placeholder')} id="message-label" >Your Message</label>
                       <textarea id="emailtext" name="emailtext" className={classNames('form-control')} rows="6" value={this.state.text} onChange={this.updateContent.bind(this)}
                               onFocus={this.inputFocus("#message-label")}>
                       </textarea>
                       <span className={classNames('material-input')}></span>
+                  </div>
+                  <div id="purposeContent" className={classNames('form-group', 'label-floating', 'is-empty', 'bmd-form-group')}>
+                      <label htmlFor="purposeContent" className={classNames('bmd-label-static')} id="radio-label" >What is this About?</label>
+                      <div class="form-check">
+                          <div class="row">
+                            <div class="col-md-4">
+                              <label class="form-check-label">
+                                  <input class="form-check-input" type="radio" name="purpose" id="radio1" value="Buying" onChange={this.updatePurpose.bind(this)} />Buying
+                                  <span class="circle">
+                                      <span class="check"></span>
+                                  </span>
+                              </label>
+                            </div>
+                            <div class="col-md-4">
+                              <label class="form-check-label">
+                                  <input class="form-check-input" type="radio" name="purpose" id="radio2" value="Selling" onChange={this.updatePurpose.bind(this)} />Selling
+                                  <span class="circle">
+                                      <span class="check"></span>
+                                  </span>
+                              </label>
+                            </div>
+                            <div class="col-md-4">
+                              <label class="form-check-label">
+                                  <input class="form-check-input" type="radio" name="purpose" id="radio3" value="Renting" onChange={this.updatePurpose.bind(this)} />Renting
+                                  <span class="circle">
+                                      <span class="check"></span>
+                                  </span>
+                              </label>
+                            </div>
+                          </div>
+                      </div>
                   </div>
               </div>
               <div className={classNames('card-footer', 'justify-content-between')}>
@@ -122,9 +194,54 @@ export default class ContactForm extends React.Component {
                           </span>
                       </label>
                   </div>
-                  <button type="submit" className={classNames('btn', 'btn-primary', 'pull-right')} onClick={this._handleSubmit.bind(this)}>Send Message</button>
+                  <button type="submit" className={classNames('btn', 'btn-primary', 'pull-right')} onClick={this._handleSubmit.bind(this)} id="submitButton">Send Message</button>
               </div>
           </form>
         );
     }
 }
+
+
+// <div class="btn-group bootstrap-select">
+//   <button type="button" class="btn dropdown-toggle bs-placeholder select-with-transition" data-toggle="dropdown" role="button" title="Single Select" aria-expanded="false">
+//     <span class="filter-option pull-left" style={{textTransform: 'none', fontSize: '14px', color: '#999999'}}>What is this about?</span>&nbsp;<span class="bs-caret"><span class="caret"></span></span>
+//   </button>
+//   <div class="dropdown-menu open" role="combobox" x-placement="bottom-start" style={{position: 'absolute', top: '41px', left: '0px', willChange: 'top, left'}}>
+//     <ul class="dropdown-menu inner" role="listbox" aria-expanded="false">
+//       <li data-original-index="1">
+//         <a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">
+//           <span class="text">General Enquiry</span>
+//           <span class="material-icons  check-mark"> done </span>
+//         </a>
+//       </li>
+//       <li data-original-index="2">
+//         <a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">
+//           <span class="text">Selling your home with Ruma</span><span class="material-icons  check-mark"> done </span>
+//         </a>
+//       </li>
+//       <li data-original-index="3">
+//         <a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">
+//           <span class="text">Buying property</span><span class="material-icons  check-mark"> done </span>
+//         </a>
+//       </li>
+//       <li data-original-index="4">
+//         <a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">
+//           <span class="text">Renting your property</span><span class="material-icons  check-mark"> done </span>
+//         </a>
+//       </li>
+//       <li data-original-index="5">
+//         <a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">
+//           <span class="text">Looking for rentals</span><span class="material-icons  check-mark"> done </span>
+//         </a>
+//       </li>
+//     </ul>
+//   </div>
+//   <select class="selectpicker" data-style="select-with-transition" title="Single Select" data-size="7" tabindex="-98">
+//     <option class="bs-title-option" value="">What is this about?</option>
+//     <option value="1">General enquiry</option>
+//     <option value="2">Selling your home with Ruma</option>
+//     <option value="3">Buying property</option>
+//     <option value="4">Renting your property</option>
+//     <option value="5">Looking for rentals</option>
+//   </select>
+// </div>
